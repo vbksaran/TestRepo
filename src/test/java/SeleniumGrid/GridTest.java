ff136617af0ea38318181a870bf181c8bebe0fe6
vbksaran <vbksaran@gmail.com>
2024-04-04 10:28:48 +0200

    cross browser test

---
+++ src/test/java/SeleniumGrid/GridTest.java
@@ -0,0 +1,28 @@
+package SeleniumGrid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GridTest {
+  @Test
+  public void testOnChrome() throws MalformedURLException, InterruptedException {
+	  ChromeOptions op=new ChromeOptions();
+	  Reporter.log("Test Executing on chrome",true);
+	  WebDriver driver=new RemoteWebDriver(new URL("http://localhost:4444"),op);
+	  Reporter.log("Diver session established with server",true);
+	  Thread.sleep(5000);
+	  driver.get("https://www.amazon.in");
+	  Thread.sleep(10000);
+	  Reporter.log("Test executng for amazon appliction",true);
+
+	  System.out.println("Title is"+driver.getTitle());
+	  driver.close();
+	  Reporter.log("Test execution on chrome completed",true);
+  }
+}
---
+++ src/test/java/SeleniumGrid/crossBrowserTest.java
@@ -0,0 +1,50 @@
+package SeleniumGrid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class crossBrowserTest {
+	public WebDriver driver;
+	@Parameters({"bname"})
+	@Test
+	public void browserTest(String bname) throws InterruptedException, MalformedURLException {
+
+		if (bname.equalsIgnoreCase("chrome")) {
+			ChromeOptions op = new ChromeOptions();
+			Reporter.log("Test Executing on "+bname , true);
+			driver = new RemoteWebDriver(new URL("http://localhost:4444"), op);
+			Reporter.log("Diver session established with server", true);
+
+		} else if (bname.equalsIgnoreCase("edge")) {
+			EdgeOptions op = new EdgeOptions();
+			Reporter.log("Test Executing on " +bname, true);
+			driver = new RemoteWebDriver(new URL("http://localhost:4444"), op);
+			Reporter.log("Diver session established with server", true);
+
+		} else if (bname.equalsIgnoreCase("firefox")) {
+			FirefoxOptions op = new FirefoxOptions();
+			Reporter.log("Test Executing on " +bname, true);
+			driver = new RemoteWebDriver(new URL("http://localhost:4444"), op);
+			Reporter.log("Diver session established with server", true);
+
+		}
+		Thread.sleep(10000);
+		Reporter.log("Test executing for amazon appliction", true);
+
+		System.out.println("Title is" + driver.getTitle());
+		driver.close();
+		Reporter.log("Test execution on chrome completed", true);
+
+	}
+
+}
